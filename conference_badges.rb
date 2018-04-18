14b52019f8fd39d2e13efa186115165bb60c8458
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-18 18:20:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,13 +3,13 @@ def badge_maker(name)
 end
 def batch_badge_creator(attendees)
   badge_messages = []
-  
+
   attendees.each do |name|
     badge_messages.push(name)
-    
+
     attendees.map do |name|
-    badge_maker(name)   
+    badge_maker(name)
   end
-badge_messages 
+badge_messages
   end
 end
