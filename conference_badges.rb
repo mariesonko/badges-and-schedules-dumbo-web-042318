b2f580cda3963981eb25ab9368534719790ed6e0
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-18 18:20:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,15 @@
 def badge_maker(name)
  return  "Hello, my name is #{name}."
 end
-def batch_badge_creator(array)
-  array.each do |name|
-    return "Hello, my name is #{name}."
+def batch_badge_creator(attendees)
+  badge_messages = []
+  
+  attendees.each do |name|
+    badge_messages.push(name)
+    
+    attendees.map do |name|
+    badge_maker(name)   
+  end
+badge_messages 
   end
 end
-batch_badge_creator
