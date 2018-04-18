69dc3308525484123a01b5c6fc9528c92896be12
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-18 18:23:02 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ def batch_badge_creator(attendees)
   badge_messages = []
 
   attendees.each do |name|
-    badge_messages.push("Hello, my name is #{name}.")
+    badge_messages<< ("Hello, my name is " + name + ".")
 
     attendees.map do |name|
     badge_maker(name)
